11f0aa557c65d6c369b7b6289453c269018f641f
Tarun <nottarun7@gmail.com>
2023-12-15 21:30:12 +0530

    Add files via upload

---
+++ assignments/week-1/01-js/hard/calculator.js
@@ -0,0 +1,57 @@
+/*
+  Implement a class `Calculator` having below methods
+    - initialise a result variable in the constructor and keep updating it after every arithmetic operation
+    - add: takes a number and adds it to the result
+    - subtract: takes a number and subtracts it from the result
+    - multiply: takes a number and multiply it to the result
+    - divide: takes a number and divide it to the result
+    - clear: makes the `result` variable to 0
+    - getResult: returns the value of `result` variable
+    - calculate: takes a string expression which can take multi-arithmetic operations and give its result
+      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
+      Points to Note: 
+        1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
+        2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
+
+  Once you've implemented the logic, test your code by running
+*/
+class Calculator {
+  constructor(){
+    let result;
+  }
+  add(num){
+    result += num;
+  }
+  sub(num){
+    result -= num;
+  }
+  mul(num){
+    result *= num;
+  }
+  div(num){
+    if (num === 0){
+      console.log("Cant divide by zero lil nigga")
+    }
+    result /= num;
+  }
+  clear(){
+    result = 0;
+  }
+
+  calculate(expression) {
+    const cleanExpression = expression.replace(/\s+/g, "");
+    if (!/^[0-9+\-*/(). ]+$/.test(cleanExpression)) {
+      throw new Error("Invalid expression");
+    }
+
+    try {
+      this.result = eval(cleanExpression);
+    } catch (error) {
+      throw new Error("Invalid expression");
+    }
+  }
+}
+
+const calculator = new Calculator();
+console.log(Calculator.calculate('10 +   2 *    (   6 - (4 + 1) / 2) + 7').get)
+module.exports = Calculator;
---
+++ assignments/week-1/01-js/medium/palindrome.js
@@ -0,0 +1,14 @@
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+*/
+
+function isPalindrome(str) {
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  const strarr = cleanedStr.split("");
+  const revstrarr = strarr.slice().reverse();
+  return strarr.join("") === revstrarr.join("");
+}
+
+module.exports = isPalindrome;
